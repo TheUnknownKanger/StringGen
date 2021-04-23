89eb4a57b5ede9a68a1d52e89b48c594ff835801
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-04-23 07:10:13 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -19,7 +19,7 @@
 import logging
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
-from bot.get_config import get_config
+from DaisyX.get_config import get_config
 
 # apparently, no error appears even if the path does not exists
 load_dotenv("config.env")
